06a31e4e360ebc98d95bf9edab4959627d556e88
Eleomar Fajutnao <109301348+Pabyala@users.noreply.github.com>
2024-09-18 20:21:25 +0800

    fix import react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter  as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./routes/Home";
 import Quiz from "./routes/Quiz";
 import Story from "./routes/About/Story";
